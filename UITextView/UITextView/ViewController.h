909a6090783e6c0d25c55fcbed4886cf987d870a
terence <wrze@sina.com>
2016-06-21 23:48:39 +0800

    demo of uitextview

---
+++ UITextView/UITextView/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  UITextView
+//
+//  Created by Terence on 16/6/21.
+//  Copyright © 2016年 Terence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property(nonatomic, retain)UITextView* textView;
+
+@end
+
